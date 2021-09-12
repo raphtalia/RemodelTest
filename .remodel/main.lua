0d8cf304a6217cd50d6c14a5b7b989c48cf6d69f
raphtalia <59540801+raphtalia@users.noreply.github.com>
2021-09-12 10:50:50 -0500

    Initial commit

---
+++ .remodel/main.lua
@@ -0,0 +1,58 @@
+local SAVED_SERVICES = {
+    "Chat",
+    "Lighting",
+    "ReplicatedFirst",
+    "ReplicatedStorage",
+    "ServerScriptService",
+    "ServerStorage",
+    "SoundService",
+    "StarterGui",
+    "StarterPack",
+    "StarterPlayer",
+    "Teams",
+    "Workspace",
+}
+
+local game = remodel.readPlaceFile("place.rbxl")
+
+local function mapAssets(directory, instance, noScripts)
+    for _,v in ipairs(remodel.readDir(directory)) do
+        if remodel.isDir(("%s/%s"):format(directory, v)) then
+            local existingAsset = instance:FindFirstChild(v)
+            if existingAsset then
+                existingAsset:Destroy()
+            end
+
+            local folder = Instance.new("Folder")
+            folder.Name = v
+            folder.Parent = instance
+
+            mapAssets(("%s/%s"):format(directory, v), folder)
+        elseif remodel.isFile(("%s/%s"):format(directory, v)) then
+            for _,asset in ipairs(remodel.readModelFile(("%s/%s"):format(directory, v))) do
+                if not noScripts or not asset:IsA("Script") then
+                    if noScripts then
+                        for _,script in ipairs(asset:GetDescendants()) do
+                            if script:IsA("Script") then
+                                script:Destroy()
+                            end
+                        end
+                    end
+
+                    asset.Parent = instance
+                end
+            end
+        end
+    end
+end
+
+local function saveService(serviceName)
+    local service = game:GetService(serviceName)
+    remodel.writeModelFile(service, ("Output/%s.rbxm"):format(serviceName))
+end
+
+mapAssets("assets/Workspace", game:GetService("Workspace"))
+
+for _,serviceName in ipairs(SAVED_SERVICES) do
+    saveService(serviceName)
+end
---
+++ README.md
@@ -0,0 +1,4 @@
+# RemodelTest
+
+This is a test repository that makes use of [Remodel](https://github.com/rojo-rbx/remodel) and
+[Rojo](https://github.com/rojo-rbx/rojo) to automatically build and upload a Roblox game upon new commits.
